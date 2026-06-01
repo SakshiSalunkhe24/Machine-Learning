86d95984136a24c1fc27628dc7e97415314fba19
SakshiSalunkhe24 <sakshianilsalunkhe2004@gmail.com>
2026-06-01 12:43:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model')
 
-df = pd.read_csv('https:/github.com/dataprofessor/data/blob/master/penguins_cleaned.csv')
+df = pd.read_csv('https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv')
